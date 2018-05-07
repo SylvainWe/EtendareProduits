1598f9eaf4d4a7ad5a7e4e3c9694f50d633aea20
sylvain <sylvain.wery1@gmail.com>
2018-05-07 12:53:40 +0200

    initial commit

---
+++ indexetendare.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Top Produits du moment</title>
+        <link title="normal" rel="stylesheet" href="styleetendare.css" type="text/css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="scriptetendare.js"></script>
+        <link href='https://fonts.googleapis.com/css?family=Limelight' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+
+    </head>
+    <body>
+        <section id="main-menu">
+            <ul>
+                <li>
+                    <div id="part1" class="images">
+                        <div class="bw"></div>
+                        <div class="color"></div>
+                    </div>
+                    <div class="description">
+                        <h2>Ruffus</h2>
+                        <h3>Ses caractéristiques</h3>
+                        <p>Champagne Belge</p>
+                        <p>Origine : Binche</p>
+                        <p>Prix : 10 euros</p>
+                    </div>
+                </li>
+                <li>
+                    <div id="part2" class="images">
+                        <div class="bw"></div>
+                        <div class="color"></div>
+                    </div>
+                    <div class="description">
+                        <h2>Curtius</h2>
+                        <h3>Ses caractéristiques</h3>
+                        <p>Bière blonde</p>
+                        <p>Origine : Liège</p>
+                        <p>Prix : 2 euros la pièce</p>
+                    </div>
+                </li>
+                <li>
+                    <div id="part3" class="images">
+                        <div class="bw"></div>
+                        <div class="color"></div>
+                    </div>
+                    <div class="description">
+                        <h2>Gin App'</h2>
+                        <h3>Ses caractéristiques</h3>
+                        <p>Gin Belge</p>
+                        <p>Origine : Genappe</p>
+                        <p>Prix : 35 euros</p>
+                    </div>
+                </li>
+                <li>
+                    <div id="part4" class="images">
+                        <div class="bw"></div>
+                        <div class="color"></div>
+                    </div>
+                    <div class="description">
+                        <h2>Pays Noir</h2>
+                        <h3>Ses caractéristiques</h3>
+                        <p>Bière blonde</p>
+                        <p>Origine : Charleroi</p>
+                        <p>Prix : 6 euros pour trois</p>
+                    </div>
+                </li>
+                <li>
+                    <div id="part5" class="images">
+                        <div class="bw"></div>
+                        <div class="color"></div>
+                    </div>
+                    <div class="description">
+                        <h2>Patachouffe</h2>
+                        <h3>Ses caractéristiques</h3>
+                        <p>Fromage belge</p>
+                        <p>Origine : Achouffe, Houffalize</p>
+                        <p>Prix : 5 euros</p>
+                    </div>
+                </li>
+            </ul>
+        </section>      
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
